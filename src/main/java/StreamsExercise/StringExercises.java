ce98c4e2de85d40ae80155c586a349223f1617f4
Timo Järvinen <timo.jarvinen@houston-inc.com>
2014-07-01 17:38:29 +0300

    Renamed class App -> StringExercises

--- src/main/java/StreamsExercise/StringExercises.java
+++ src/main/java/StreamsExercise/StringExercises.java
@@ -3,7 +3,7 @@ package StreamsExercise;
 import java.util.List;
 import java.util.Optional;
 
-public class App {
+public class StringExercises {
 
     public Optional<String> transformUppercaseAndConcatenateWithoutMap(List<String> words) {
         return words.stream().reduce((x, y) -> x.toUpperCase().concat(y.toUpperCase()));
--- src/test/java/StreamsExercise/StringExercisesTest.java
+++ src/test/java/StreamsExercise/StringExercisesTest.java
@@ -1,31 +1,30 @@
 package StreamsExercise;
 
 import junit.framework.TestCase;
-import org.junit.Before;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class AppTest extends TestCase {
-    private App app;
+public class StringExercisesTest extends TestCase {
+    private StringExercises stringExercises;
     private List<String> words;
 
     @Override
     protected void setUp() {
-        app = new App();
+        stringExercises = new StringExercises();
         words = Arrays.asList("hi", "hello", "shalom", "tov");
     }
 
     public void testReturnUppercaseAndConcatenatedStringWithoutMap() {
-        assertEquals("HIHELLOSHALOMTOV", app.transformUppercaseAndConcatenateWithoutMap(words).get());
+        assertEquals("HIHELLOSHALOMTOV", stringExercises.transformUppercaseAndConcatenateWithoutMap(words).get());
     }
 
     public void testReturnUppercaseAndConcatenatedStringWithMap() {
-        assertEquals("HIHELLOSHALOMTOV", app.transformUppercaseAndConcatenateWithMap(words).get());
+        assertEquals("HIHELLOSHALOMTOV", stringExercises.transformUppercaseAndConcatenateWithMap(words).get());
     }
 
     public void testConcatenateWithCommas() {
-        assertEquals("hi,hello,shalom,tov", app.concatenateWithCommas(words).get());
+        assertEquals("hi,hello,shalom,tov", stringExercises.concatenateWithCommas(words).get());
     }
 
     public void thisShouldNotBeCalled() {
