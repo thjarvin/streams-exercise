ada2f0bc2461ef7b5bb61c3268b01cb6189bcf54
Timo Järvinen <timo.jarvinen@houston-inc.com>
2014-07-02 15:07:31 +0300

    Added method orderedNumberList

--- src/main/java/StreamsExercise/NumberExercises.java
+++ src/main/java/StreamsExercise/NumberExercises.java
@@ -6,12 +6,20 @@ import java.util.stream.Stream;
 
 public class NumberExercises {
 
-    public Stream<Double> randomNumberStream(int size) {
+    private Stream<Double> randomNumberStream(int size) {
         return(Stream.generate(() -> getRandomDouble()).limit(size));
     }
 
     public List<Double> randomNumberList(int size) {
         return(randomNumberStream(size).collect(Collectors.toList()));
+    }
+
+    private Stream<Integer> orderedNumberStream(int first, int stepSize, int listSize) {
+        return (Stream.iterate(first, s -> s + stepSize)).limit(listSize);
+    }
+
+    public List<Integer> orderedNumberList(int first, int stepSize, int listSize) {
+        return (orderedNumberStream(first, stepSize, listSize).collect(Collectors.toList()));
     }
 
     public double getRandomDouble() {
--- src/test/java/StreamsExercise/NumberExercisesTest.java
+++ src/test/java/StreamsExercise/NumberExercisesTest.java
@@ -6,13 +6,13 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 public class NumberExercisesTest extends TestCase {
-    private NumberExercises integerExercises;
+    private NumberExercises numberExercises;
     private int i = 1;
     private DecimalFormat df = new DecimalFormat("#.0");
 
     @Override
     protected void setUp() {
-        integerExercises = new NumberExercises() {
+        numberExercises = new NumberExercises() {
 
             @Override
             public double getRandomDouble() {
@@ -22,10 +22,19 @@ public class NumberExercisesTest extends TestCase {
     }
 
     public void testRandomNumberList() {
-        List<Double> randomNumberList = integerExercises.randomNumberList(3);
+        List<Double> randomNumberList = numberExercises.randomNumberList(3);
         assertEquals(3, randomNumberList.size());
         assertEquals(0.1, randomNumberList.get(0));
         assertEquals(0.2, randomNumberList.get(1));
         assertEquals(0.3, randomNumberList.get(2));
+    }
+
+    public void testOrderedNumberList() {
+        List<Integer> orderedNumberList = numberExercises.orderedNumberList(50, 5, 4);
+        assertEquals(4, orderedNumberList.size());
+        assertEquals(Integer.valueOf(50), orderedNumberList.get(0));
+        assertEquals(Integer.valueOf(55), orderedNumberList.get(1));
+        assertEquals(Integer.valueOf(60), orderedNumberList.get(2));
+        assertEquals(Integer.valueOf(65), orderedNumberList.get(3));
     }
 }
